510ae8c53e035fdba908a47f2ea1fd82a7ef85bc
Genetics&Genomics_Lab <35423313+charleslywang@users.noreply.github.com>
2026-03-22 10:19:13 +0800

    2026.3.22

--- HAUpekinT2T/genome_assessment/QV.sh
+++ HAUpekinT2T/genome_assessment/QV.sh
@@ -2,5 +2,5 @@
 
 meryl k=21 threads=128 memory=500g  count NGS_1.fq.gz output fq1.meryl
 meryl k=21 threads=128 memory=500g  count NGS_2.fq.gz output fq2.meryl
-
- meryl union-sum output k21.meryl fq*.meryl
+meryl union-sum output k21.meryl fq*.meryl
+merqury.sh k21.meryl HAUpekinT2T.fa HAUpekinT2T.qv
